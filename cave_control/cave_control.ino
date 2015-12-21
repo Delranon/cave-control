838111e153b791dd70cb55d04409cc84eac90a11
Christopher Mikosch <chmik@dtnet.co.at>
2015-12-21 15:08:51 +0100

    Real Use
    Aufgeteilt in Workshop und PC-Bereich
    Mehr Multi Szenarien
    Todo: Clean-Up und Code-Verbesserungen

--- cave_control/cave_control.ino
+++ cave_control/cave_control.ino
@@ -1,9 +1,8 @@
 /*********************************** INFO **************************************\
-| 16 Button TTP229-LSF TouchPanel schaltet 12 Relais (10A 240V)für die          |
-| Beleuchtung. Taste 1 - 12 die einzelnen Relais, 13 alles ein - mit 2 Sekunden |
-| Last Verzögerung - Taste 16 alles aus - auch mit 2 Sekunden Last-Verzögerung  |
-\******************************************************************************/
-/*********************************** CIRCUIT **********************************\
+| 16 Button TTP229-LSF TouchPanel schaltet 9 Relais (10A 240V)für die           |
+| Beleuchtung.                                                                  |
+\*******************************************************************************/
+/*********************************** CIRCUIT ***********************************\
 | TouchPanel:                                                                   |
 | VCC           5V                                                              |
 | SCL           Pin 3                                                           |
@@ -14,6 +13,13 @@
 | VCC           5V                                                              |
 | Relais        Pin Pin 22 - Pin 45                                             |
 |                                                                               |
+\*******************************************************************************/
+/****************************** Tastenbelegung ********************************\
+/ 1 2 3       Licht an (Elektro - Modellbau - 3D Druck)   4 Licht aus          |
+/ 5 6         Licht an (Main-PC - Sec-PC)                 8 Licht aus          |
+/ 9 10 11 12  Monitor an  (1 - 4)                                              |
+/ 13          Alles an (Demo Modus)                                            |
+/ 16          Alles aus                                                        |
 \******************************************************************************/
 
 #include <TTP229.h>
@@ -23,19 +29,15 @@ const int SDO_PIN = 2;  // The pin number of the data pin.
 
 TTP229 ttp229(SCL_PIN, SDO_PIN); // TTP229(sclPin, sdoPin)
 
-
-int Relais1 = 22;
-int Relais2 = 24;
-int Relais3 = 26;
-int Relais4 = 28;
-int Relais5 = 45;
-int Relais6 = 32;
-int Relais7 = 34;
-int Relais8 = 36;
-int Relais9 = 38;
-int Relais10 = 40;
-int Relais11 = 42;
-int Relais12 = 44;
+int Relais1 = 22;   // Licht Main-PC
+int Relais2 = 24;   // Licht Sec-PC
+int Relais3 = 26;   // Licht Elektro
+int Relais4 = 28;   // Licht Modellbau
+int Relais5 = 45;   // Licht 3D Druck
+int Relais6 = 32;   // Monitor 1
+int Relais7 = 34;   // Monitor 2
+int Relais8 = 36;   // Monitor 3
+int Relais9 = 38;   // Monitor 4
 
 int Relais1State = 0;
 int Relais2State = 0;
@@ -46,10 +48,6 @@ int Relais6State = 0;
 int Relais7State = 0;
 int Relais8State = 0;
 int Relais9State = 0;
-int Relais10State = 0;
-int Relais11State = 0;
-int Relais12State = 0;
-
 
 void setup() {
 
@@ -66,9 +64,6 @@ pinMode(Relais7, OUTPUT);
 pinMode(Relais8, OUTPUT);
 
 pinMode(Relais9, OUTPUT);
-pinMode(Relais10, OUTPUT);
-pinMode(Relais11, OUTPUT);
-pinMode(Relais12, OUTPUT);
 
 digitalWrite(Relais1, HIGH);
 digitalWrite(Relais2, HIGH);
@@ -81,211 +76,184 @@ digitalWrite(Relais7, HIGH);
 digitalWrite(Relais8, HIGH);
 
 digitalWrite(Relais9, HIGH);
-digitalWrite(Relais10, HIGH);
-digitalWrite(Relais11, HIGH);
-digitalWrite(Relais12, HIGH);
-
-
 }
 
 void loop() 
 {
 
   uint8_t key = ttp229.ReadKey16();
-  if (key) 
+  if (key)
   {
    //Serial.println(key);
-   if (key == 1) 
-      { if (Relais1State == 0)
-        { digitalWrite(Relais1, LOW); 
-          Relais1State = 1;
-        } else
-        { digitalWrite(Relais1, HIGH); 
-          Relais1State = 0;
-        }
-      }
-   if (key == 2) 
-      { if (Relais2State == 0)
-        { digitalWrite(Relais2, LOW); 
-          Relais2State = 1;
-        } else
-        { digitalWrite(Relais2, HIGH); 
-          Relais2State = 0;
-        }
-      }
-   if (key == 3) 
+   if (key == 1)
       { if (Relais3State == 0)
-        { digitalWrite(Relais3, LOW); 
+        { digitalWrite(Relais3, LOW);
           Relais3State = 1;
         } else
-        { digitalWrite(Relais3, HIGH); 
+        { digitalWrite(Relais3, HIGH);
           Relais3State = 0;
         }
       }
-   if (key == 4) 
+   if (key == 2)
       { if (Relais4State == 0)
-        { digitalWrite(Relais4, LOW); 
+        { digitalWrite(Relais4, LOW);
           Relais4State = 1;
         } else
-        { digitalWrite(Relais4, HIGH); 
+        { digitalWrite(Relais4, HIGH);
           Relais4State = 0;
         }
       }
-   if (key == 5) 
+   if (key == 3)
       { if (Relais5State == 0)
-        { digitalWrite(Relais5, LOW); 
+        { digitalWrite(Relais5, LOW);
           Relais5State = 1;
         } else
-        { digitalWrite(Relais5, HIGH); 
+        { digitalWrite(Relais5, HIGH);
           Relais5State = 0;
         }
       }
-   if (key == 6) 
+   if (key == 4)
+        { workshopoff(1000);    }
+   if (key == 5)
+      { if (Relais1State == 0)
+        { digitalWrite(Relais1, LOW);
+          Relais1State = 1;
+        } else
+        { digitalWrite(Relais1, HIGH);
+          Relais1State = 0;
+        }
+      }
+   if (key == 6)
+      { if (Relais2State == 0)
+        { digitalWrite(Relais2, LOW);
+          Relais2State = 1;
+        } else
+        { digitalWrite(Relais2, HIGH);
+          Relais2State = 0;
+        }
+      }
+     if (key == 8)
+        { pclightoff(1000);    }
+   if (key == 9)
       { if (Relais6State == 0)
-        { digitalWrite(Relais6, LOW); 
+        { digitalWrite(Relais6, LOW);
           Relais6State = 1;
         } else
-        { digitalWrite(Relais6, HIGH); 
+        { digitalWrite(Relais6, HIGH);
           Relais6State = 0;
         }
       }
-   if (key == 7) 
-      { if (Relais7State == 0)
-        { digitalWrite(Relais7, LOW); 
+   if (key == 10)
+       { if (Relais7State == 0)
+        { digitalWrite(Relais7, LOW);
           Relais7State = 1;
         } else
-        { digitalWrite(Relais7, HIGH); 
+        { digitalWrite(Relais7, HIGH);
           Relais7State = 0;
         }
-      }
-   if (key == 8) 
-      { if (Relais8State == 0)
-        { digitalWrite(Relais8, LOW); 
-          Relais8State = 1;
-        } else
-        { digitalWrite(Relais8, HIGH); 
-          Relais8State = 0;
-        }
-      }
-   if (key == 9) 
-      { if (Relais9State == 0)
-        { digitalWrite(Relais9, LOW); 
+       }
+   if (key == 11)
+       { if (Relais8State == 0)
+         { digitalWrite(Relais8, LOW);
+           Relais8State = 1;
+         } else
+         { digitalWrite(Relais8, HIGH);
+           Relais8State = 0;
+         }
+       }
+   if (key == 12)
+       { if (Relais9State == 0)
+        { digitalWrite(Relais9, LOW);
           Relais9State = 1;
         } else
-        { digitalWrite(Relais9, HIGH); 
+        { digitalWrite(Relais9, HIGH);
           Relais9State = 0;
         }
-      }
-   if (key == 10) 
-      { if (Relais10State == 0)
-        { digitalWrite(Relais10, LOW); 
-          Relais10State = 1;
-        } else
-        { digitalWrite(Relais10, HIGH); 
-          Relais10State = 0;
-        }
-      }
-   if (key == 11) 
-      { if (Relais11State == 0)
-        { digitalWrite(Relais11, LOW); 
-          Relais11State = 1;
-        } else
-        { digitalWrite(Relais11, HIGH); 
-          Relais11State = 0;
-        }
-      }
-   if (key == 12) 
-      { if (Relais12State == 0)
-        { digitalWrite(Relais12, LOW); 
-          Relais12State = 1;
-        } else
-        { digitalWrite(Relais12, HIGH); 
-          Relais12State = 0;
-        }
-      }
-    if (key == 13) 
-      { allon();    }
-  if (key == 16) 
-       { alloff();    }
+       }
+   if (key == 13)
+       { allon(2000);    }
+   if (key == 16)
+      { alloff(1000);    }
   }
-
-
 }
 
-void allon()
+void workshopoff(int pause)
 {
-  digitalWrite(Relais1, LOW); 
-  Relais1State = 1;
-  delay(2000);
-  digitalWrite(Relais2, LOW); 
-  Relais2State = 1;
-  delay(2000);
-  digitalWrite(Relais3, LOW); 
-  Relais3State = 1;
-  delay(2000);
-  digitalWrite(Relais4, LOW); 
-  Relais4State = 1;
-  delay(2000);
-  digitalWrite(Relais5, LOW); 
-  Relais5State = 1;
-  delay(2000);
-  digitalWrite(Relais6, LOW); 
-  Relais6State = 1;
-  delay(2000);
-  digitalWrite(Relais7, LOW); 
-  Relais7State = 1;
-  delay(2000);
-  digitalWrite(Relais8, LOW); 
-  Relais8State = 1;
-  delay(2000);
-  digitalWrite(Relais9, LOW); 
-  Relais9State = 1;
-  delay(2000);
-  digitalWrite(Relais10, LOW); 
-  Relais10State = 1;
-  delay(2000);
-  digitalWrite(Relais11, LOW); 
-  Relais11State = 1;
-  delay(2000);
-  digitalWrite(Relais12, LOW); 
-  Relais12State = 1;
+  digitalWrite(Relais3, HIGH);
+  Relais3State = 0;
+  delay(pause);
+  digitalWrite(Relais4, HIGH);
+  Relais4State = 0;
+  delay(pause);
+  digitalWrite(Relais5, HIGH);
+  Relais5State = 0;
 }
 
-void alloff()
-{ 
-  digitalWrite(Relais1, HIGH); 
+void pclightoff(int pause)
+{
+  digitalWrite(Relais1, HIGH);
   Relais1State = 0;
-  delay(2000);
-  digitalWrite(Relais2, HIGH); 
+  delay(pause);
+  digitalWrite(Relais2, HIGH);
   Relais2State = 0;
-  delay(2000);
-  digitalWrite(Relais3, HIGH); 
+}
+
+void allon(int pause)
+{
+  digitalWrite(Relais1, LOW);
+  Relais1State = 1;
+  delay(pause);
+  digitalWrite(Relais2, LOW);
+  Relais2State = 1;
+  delay(pause);
+  digitalWrite(Relais3, LOW);
+  Relais3State = 1;
+  delay(pause);
+  digitalWrite(Relais4, LOW);
+  Relais4State = 1;
+  delay(pause);
+  digitalWrite(Relais5, LOW);
+  Relais5State = 1;
+  delay(pause);
+  digitalWrite(Relais6, LOW);
+  Relais6State = 1;
+  delay(pause);
+  digitalWrite(Relais7, LOW);
+  Relais7State = 1;
+  delay(pause);
+  digitalWrite(Relais8, LOW);
+  Relais8State = 1;
+  delay(pause);
+  digitalWrite(Relais9, LOW);
+  Relais9State = 1;
+}
+
+void alloff(int pause)
+{
+  digitalWrite(Relais1, HIGH);
+  Relais1State = 0;
+  delay(pause);
+  digitalWrite(Relais2, HIGH);
+  Relais2State = 0;
+  delay(pause);
+  digitalWrite(Relais3, HIGH);
   Relais3State = 0;
-  delay(2000);
-  digitalWrite(Relais4, HIGH); 
+  delay(pause);
+  digitalWrite(Relais4, HIGH);
   Relais4State = 0;
-  delay(2000);
-  digitalWrite(Relais5, HIGH); 
+  delay(pause);
+  digitalWrite(Relais5, HIGH);
   Relais5State = 0;
-  delay(2000);
-  digitalWrite(Relais6, HIGH); 
+  delay(pause);
+  digitalWrite(Relais6, HIGH);
   Relais6State = 0;
-  delay(2000);
-  digitalWrite(Relais7, HIGH); 
+  delay(pause);
+  digitalWrite(Relais7, HIGH);
   Relais7State = 0;
-  delay(2000);
-  digitalWrite(Relais8, HIGH); 
+  delay(pause);
+  digitalWrite(Relais8, HIGH);
   Relais8State = 0;
-  delay(2000);
-  digitalWrite(Relais9, HIGH); 
+  delay(pause);
+  digitalWrite(Relais9, HIGH);
   Relais9State = 0;
-  delay(2000);
-  digitalWrite(Relais10, HIGH); 
-  Relais10State = 0;
-  delay(2000);
-  digitalWrite(Relais11, HIGH); 
-  Relais11State = 0;
-  delay(2000);
-  digitalWrite(Relais12, HIGH); 
-  Relais12State = 0;
 }
